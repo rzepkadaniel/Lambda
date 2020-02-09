66bef33bfdaecbe633968f1df4551ecffa256ebc
rzepkadaniel <Czerwony5!>
2020-02-09 15:42:12 +0100

    lambda

--- src/Filtrowanie.java
+++ src/Filtrowanie.java
@@ -6,21 +6,37 @@ public class Filtrowanie {
 
     public static void main(String[] args) {
         List<Person> people = Arrays.asList(
-                new Person("jan1", "Kowalski1"),
+                new Person("aan1", "Kowalski1"),
                 new Person("jan4", "owalski4"),
-                new Person("jan2", "Kowalski1"),
+                new Person("oan2", "Kowalski1"),
                 new Person("jan3", "Kowalski3"),
-                new Person("jan5", "Kowalski5")
+                new Person("jarek", "Kowalski5")
 
         );
         // Strumien z kolekcji
 
-        final Predicate<String> zaczynaSieNaKPredicate = s -> s.startsWith("K");
+//        final Predicate<String> zaczynaSieNaKPredicate = s -> s.startsWith("K");
+//        people
+//                .stream()
+//                .map(person -> person.lastName)
+//                .filter(zaczynaSieNaKPredicate)
+//                .forEach(s -> System.out.println(s));
+        // utworzymy posortowana liczbe stringow (name + lastname z Person
+        // odfiltrujemy zaczynajace sie na samogloske
+        // stringi calosc wielka litera
+
+        final Predicate<String> zaczynaSieSamogloska = s -> !s.startsWith("a");
         people
                 .stream()
-                .map(person -> person.lastName)
-                .filter(zaczynaSieNaKPredicate)
-                .forEach(s -> System.out.println(s));
+                .map(person -> person.name + " " + person.lastName)
+                .filter(zaczynaSieSamogloska)
+                .sorted()
+                .forEach(s -> System.out.println(s.toUpperCase()));
+
+
+
+
+
 
 //        people.forEach(person -> System.out.println(person.name));
 //        people.forEach(person -> System.out.println(person.lastName));
